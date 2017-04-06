8ad77316f4697ee961e6648e54363bbc7f214f52
Dulanja Wijethunga <dulanja.wijethunga@trycake.com>
2017-04-06 23:42:23 +0530

    added configuration

--- src/main/java/com/example/OrderApplication.java
+++ src/main/java/com/example/OrderApplication.java
@@ -1,7 +1,19 @@
 package com.example;
 
+import io.dropwizard.Application;
+import io.dropwizard.setup.Environment;
+
 /**
  * @author by Dulanja Wijethunga.
  */
-public class OrderApplication {
+public class OrderApplication extends Application<OrderConfiguration> {
+
+    public static void main(String args[]) throws Exception {
+        new OrderApplication().run(args);
+    }
+
+    @Override
+    public void run(OrderConfiguration configuration, Environment environment) throws Exception {
+
+    }
 }
--- src/main/java/com/example/OrderConfiguration.java
+++ src/main/java/com/example/OrderConfiguration.java
@@ -1,7 +1,18 @@
 package com.example;
 
+import io.dropwizard.Configuration;
+
 /**
  * @author by Dulanja Wijethunga.
  */
-public class OrderConfiguration {
+public class OrderConfiguration extends Configuration {
+    private String demoApp;
+
+    public String getDemoApp() {
+        return demoApp;
+    }
+
+    public void setDemoApp(String demoApp) {
+        this.demoApp = demoApp;
+    }
 }
